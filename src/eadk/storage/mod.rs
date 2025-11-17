d6984b4ca84c2630f6ccbc4a1a0c9c10323edd08
Yannis <52321382+yannis300307@users.noreply.github.com>
2025-11-17 16:49:20 +0100

    Cleaner multi target functions

--- src/eadk/storage/mod.rs
+++ src/eadk/storage/mod.rs
@@ -1,136 +1,143 @@
-#[cfg(target_os = "none")]
-use core::ffi::CStr;
-
-#[cfg(target_os = "none")]
-use alloc::{ffi, string::String, vec::Vec};
+calc_use!(core::ffi::CStr);
+calc_use!(alloc::ffi);
+calc_use!(alloc::string::String);
+calc_use!(alloc::vec::Vec);
 
 #[cfg(not(target_os = "none"))]
 use std::fs;
 
-#[cfg(target_os = "none")]
 pub fn file_write(filename: &str, content: &[u8]) -> Option<()> {
-    let c_string = ffi::CString::new(filename).unwrap();
-    let result = unsafe { extapp_fileWrite(c_string.as_ptr(), content.as_ptr(), content.len()) };
-    if result { Some(()) } else { None }
-}
-
-#[cfg(not(target_os = "none"))]
-pub fn file_write(filename: &str, content: &[u8]) -> Option<()> {
-    if !fs::exists("simulator/storage").ok()? {
-        fs::create_dir_all("simulator/storage").ok()?;
+    #[cfg(target_os = "none")]
+    {
+        let c_string = ffi::CString::new(filename).unwrap();
+        let result =
+            unsafe { extapp_fileWrite(c_string.as_ptr(), content.as_ptr(), content.len()) };
+        if result { Some(()) } else { None }
     }
-    fs::write(format!("simulator/storage/{}", filename), content).ok()
-}
-
-#[cfg(target_os = "none")]
-pub fn file_exists(filename: &str) -> bool {
-    let c_string = ffi::CString::new(filename).unwrap();
-    unsafe { extapp_fileExists(c_string.as_ptr()) }
-}
-
-#[cfg(not(target_os = "none"))]
-pub fn file_exists(filename: &str) -> bool {
-    fs::exists(format!("simulator/storage/{}", filename)).unwrap()
-}
-
-#[cfg(target_os = "none")]
-pub fn file_read(filename: &str) -> Option<Vec<u8>> {
-    let c_string = ffi::CString::new(filename).unwrap();
-    let mut lenght: usize = 0;
-    let array_pointer = unsafe { extapp_fileRead(c_string.as_ptr(), &mut lenght as *mut usize) };
-
-    if array_pointer.is_null() {
-        return None;
+    #[cfg(not(target_os = "none"))]
+    {
+        if !fs::exists("simulator/storage").ok()? {
+            fs::create_dir_all("simulator/storage").ok()?;
+        }
+        fs::write(format!("simulator/storage/{}", filename), content).ok()
     }
-
-    Some(unsafe { core::slice::from_raw_parts(array_pointer, lenght).to_vec() })
 }
 
-#[cfg(not(target_os = "none"))]
+pub fn file_exists(filename: &str) -> bool {
+    #[cfg(target_os = "none")]
+    {
+        let c_string = ffi::CString::new(filename).unwrap();
+        unsafe { extapp_fileExists(c_string.as_ptr()) }
+    }
+    #[cfg(not(target_os = "none"))]
+    {
+        fs::exists(format!("simulator/storage/{}", filename)).unwrap()
+    }
+}
+
 pub fn file_read(filename: &str) -> Option<Vec<u8>> {
-    fs::read(format!("simulator/storage/{}", filename)).ok()
+    #[cfg(target_os = "none")]
+    {
+        let c_string = ffi::CString::new(filename).unwrap();
+        let mut lenght: usize = 0;
+        let array_pointer =
+            unsafe { extapp_fileRead(c_string.as_ptr(), &mut lenght as *mut usize) };
+
+        if array_pointer.is_null() {
+            return None;
+        }
+
+        Some(unsafe { core::slice::from_raw_parts(array_pointer, lenght).to_vec() })
+    }
+    #[cfg(not(target_os = "none"))]
+    {
+        fs::read(format!("simulator/storage/{}", filename)).ok()
+    }
 }
 
-#[cfg(target_os = "none")]
 pub fn read_file_slice(filename: &str, start: usize, mut slice_lenght: usize) -> Option<Vec<u8>> {
-    let c_string = ffi::CString::new(filename).unwrap();
-    let mut lenght: usize = 0;
-    let array_pointer = unsafe {
-        extapp_fileRead(
-            c_string.as_ptr().offset(slice_lenght as isize),
-            &mut lenght as *mut usize,
-        )
-    };
+    #[cfg(target_os = "none")]
+    {
+        let c_string = ffi::CString::new(filename).unwrap();
+        let mut lenght: usize = 0;
+        let array_pointer = unsafe {
+            extapp_fileRead(
+                c_string.as_ptr().offset(slice_lenght as isize),
+                &mut lenght as *mut usize,
+            )
+        };
 
-    if array_pointer.is_null() {
-        return None;
+        if array_pointer.is_null() {
+            return None;
+        }
+
+        if lenght < slice_lenght {
+            slice_lenght = lenght;
+        }
+
+        Some(unsafe { core::slice::from_raw_parts(array_pointer, slice_lenght).to_vec() })
     }
-
-    if lenght < slice_lenght {
-        slice_lenght = lenght;
+    #[cfg(not(target_os = "none"))]
+    {
+        fs::read(format!("simulator/storage/{}", filename))
+            .map(|v| v[start..(start + slice_lenght)].to_vec())
+            .ok()
     }
-
-    Some(unsafe { core::slice::from_raw_parts(array_pointer, slice_lenght).to_vec() })
 }
 
-#[cfg(not(target_os = "none"))]
-pub fn read_file_slice(filename: &str, start: usize, slice_lenght: usize) -> Option<Vec<u8>> {
-    fs::read(format!("simulator/storage/{}", filename))
-        .map(|v| v[start..(start + slice_lenght)].to_vec())
-        .ok()
-}
-
-#[cfg(target_os = "none")]
 pub fn file_erase(filename: &str) -> Option<()> {
-    let c_string = ffi::CString::new(filename).unwrap();
-    let result = unsafe { extapp_fileErase(c_string.as_ptr()) };
-    if result { Some(()) } else { None }
+    #[cfg(target_os = "none")]
+    {
+        let c_string = ffi::CString::new(filename).unwrap();
+        let result = unsafe { extapp_fileErase(c_string.as_ptr()) };
+        if result { Some(()) } else { None }
+    }
+    #[cfg(not(target_os = "none"))]
+    {
+        fs::remove_file(format!("simulator/storage/{}", filename)).ok()
+    }
 }
 
-#[cfg(not(target_os = "none"))]
-pub fn extapp_file_erase(filename: &str) -> Option<()> {
-    fs::remove_file(format!("simulator/storage/{}", filename)).ok()
-}
-
-#[cfg(target_os = "none")]
 pub fn file_list_with_extension(max_records: usize, extension: &str) -> Vec<String> {
-    let mut filenames: Vec<*mut u8> = Vec::with_capacity(max_records);
-    let c_string = ffi::CString::new(extension).unwrap();
+    #[cfg(target_os = "none")]
+    {
+        let mut filenames: Vec<*mut u8> = Vec::with_capacity(max_records);
+        let c_string = ffi::CString::new(extension).unwrap();
 
-    unsafe {
-        let final_len = extapp_fileListWithExtension(
-            filenames.as_mut_slice().as_mut_ptr(),
-            max_records as isize,
-            c_string.as_ptr(),
-        );
-        filenames.set_len(final_len as usize);
+        unsafe {
+            let final_len = extapp_fileListWithExtension(
+                filenames.as_mut_slice().as_mut_ptr(),
+                max_records as isize,
+                c_string.as_ptr(),
+            );
+            filenames.set_len(final_len as usize);
 
+            let mut files: Vec<String> = Vec::new();
+            for name_ptr in filenames {
+                if !name_ptr.is_null() {
+                    let name = CStr::from_ptr(name_ptr).to_string_lossy().into_owned();
+                    files.push(name);
+                }
+            }
+
+            files
+        }
+    }
+    #[cfg(not(target_os = "none"))]
+    {
         let mut files: Vec<String> = Vec::new();
-        for name_ptr in filenames {
-            if !name_ptr.is_null() {
-                let name = CStr::from_ptr(name_ptr).to_string_lossy().into_owned();
-                files.push(name);
+        for entry in fs::read_dir("simulator/storage").unwrap() {
+            if files.len() > max_records {
+                let entry = entry.unwrap();
+                let name = entry.file_name().into_string().unwrap();
+                if name.ends_with(extension) {
+                    files.push(name);
+                }
             }
         }
 
         files
     }
-}
-
-#[cfg(not(target_os = "none"))]
-pub fn file_list_with_extension(max_records: usize, extension: &str) -> Vec<String> {
-    let mut files: Vec<String> = Vec::new();
-    for entry in fs::read_dir("simulator/storage").unwrap() {
-        if files.len() > max_records {
-            let entry = entry.unwrap();
-            let name = entry.file_name().into_string().unwrap();
-            if name.ends_with(extension) {
-                files.push(name);
-            }
-        }
-    }
-
-    files
 }
 
 pub enum CalculatorModel {
@@ -140,18 +147,19 @@ pub enum CalculatorModel {
     Simulator,
 }
 
-#[cfg(target_os = "none")]
 pub fn get_calculator_model() -> CalculatorModel {
-    match unsafe { extapp_calculatorModel() } {
-        _ => CalculatorModel::Unknown,
-        1 => CalculatorModel::N0110N0115,
-        2 => CalculatorModel::N0120,
+    #[cfg(target_os = "none")]
+    {
+        match unsafe { extapp_calculatorModel() } {
+            _ => CalculatorModel::Unknown,
+            1 => CalculatorModel::N0110N0115,
+            2 => CalculatorModel::N0120,
+        }
     }
-}
-
-#[cfg(not(target_os = "none"))]
-pub fn get_calculator_model() -> CalculatorModel {
-    CalculatorModel::Simulator
+    #[cfg(not(target_os = "none"))]
+    {
+        CalculatorModel::Simulator
+    }
 }
 
 #[cfg(target_os = "none")]
